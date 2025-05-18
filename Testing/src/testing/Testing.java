fac64932cb43eeb5d024aa3c1054aa7181cc6476
Hamza-M06 <hwmohamed7@gmail.com>
2025-05-18 16:06:49 -0400

    Update Testing.java

--- Testing/src/testing/Testing.java
+++ Testing/src/testing/Testing.java
@@ -17,6 +17,7 @@ public class Testing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    public static void(String[] args) {
         // TODO code application logic here
     }
 
